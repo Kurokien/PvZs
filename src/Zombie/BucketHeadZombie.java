d069a7c3f29ad354b54a6ace7189abdd420694d9
Kurokien <124993172+Kurokien@users.noreply.github.com>
2023-12-27 11:16:37 +0700

    Create BucketHeadZombie.java

---
+++ src/Zombie/BucketHeadZombie.java
@@ -0,0 +1,10 @@
+package Zombie;
+import GUI.GamePanel;
+
+public class BucketHeadZombie extends Zombie {
+
+    public BucketHeadZombie(GamePanel parent,int lane){
+        super(parent,lane);
+        health = 2500;
+    }
+}
