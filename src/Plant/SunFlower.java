fd12ba1fb5a1caab24420cde4ca6f1ee53b14d04
Kurokien <nguyenminhkhoa0169@gmail.com>
2024-01-06 22:10:19 +0700

    Create SunFlower.java

---
+++ src/Plant/SunFlower.java
@@ -0,0 +1,9 @@
+package Plant;
+    public class SunFlower extends Plant {
+
+        public SunFlower(int x, int y, int id, int plantType) {
+            super(x ,y ,id , sunFlower );
+        }
+    
+    }
+
